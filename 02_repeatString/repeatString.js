a7484b383c0320d1ae4864936d82b80f4975e93f
Jon Kang <jonkang@jons-mbp.myfiosgateway.com>
2022-07-06 21:10:03 -0400

    finish 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (text, num) {
+  let result = "";
+  if (num === 0) return result;
+  if (num === -1) return "ERROR";
+  if (text === "") return result;
+  for (let i = 0; i < num; i++) {
+    result += text;
+  }
 
+  return result;
 };
 
 // Do not edit below this line
